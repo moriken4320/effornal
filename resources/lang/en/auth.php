7353bc3d775448559ca9424b955dc1430382563a
moriken4320 <moriken.4320@gmail.com>
2021-01-20 23:44:32 +0900

    ログイン時のエラ〜メッセージを日本語化

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'ログイン情報が登録されていません。',
+    'throttle' => 'ログイン試行回数が多すぎます。:seconds秒後にもう一度お試しください。',
 
 ];
