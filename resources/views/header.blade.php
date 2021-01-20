a94fbd7b6cd72e615a1c7ff4179bf1a4be728718
moriken4320 <moriken.4320@gmail.com>
2021-01-20 14:47:46 +0900

    ヘッダー更新

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,32 +1,11 @@
 @section('header')
-<h1>header</h1>
-    {{-- <nav class="navbar navbar-expand-lg navbar-light">
-      <div class="container">
-        <a class="navbar__brand navbar__mainLogo" href="/"></a>
 
-        @if (Auth::check())
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-md-auto align-items-center">
-              <li>
-                <a class="btn btn-primary" href="/posts/new">投稿</a>
-              </li>
-              <li>
-                <a class="nav-link commonNavIcon profile-icon" href="/users/{{ Auth::user()->id }}"></a>
-              </li>
-            </ul>
-          </div>
-        @else
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-md-auto align-items-center">
-            <li>
-              <a class="btn btn-primary" href="{{ route('login') }}">ログイン</a>
-            </li>
-          </ul>
-        </div>
-        @endif
+    <nav class="navbar navbar-expand navbar-dark shadow-none">
+
+      <div class="row no-gutters">
+        <a class="navbar-brand font-weight-bold shadow-none" href="/">Effornal</a>
       </div>
-    </nav> --}}
+    
+    </nav>
+
 @endsection
