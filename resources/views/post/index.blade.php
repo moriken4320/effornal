754ca31ccf99d3f2a031860fd1309414afff93c4
moriken4320 <moriken.4320@gmail.com>
2021-01-22 01:34:14 +0900

    投稿一覧画面のビュー作成

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -6,22 +6,8 @@
 
 @section('content')
 <div class="container">
-  <p>post.index</p>
-  <a href="#" class="btn btn-primary">仮のボタンです</a>
-  @guest
-  ログインしていない
-  @endguest
-  @auth
-  ログイン中
-  @endauth
-
   @foreach ($posts as $post)
-      <p>{{ $post->user->name }}</p>
-      <p>{{ $post->subject->name }}</p>
-      <p>{{ $post->study_time }}</p>
-      <p>{{ $post->text }}</p>
-      <p>{{ $post->created_at }}</p>
-      <hr>
+      @include('post.post_card')
   @endforeach
 </div>
 @endsection
