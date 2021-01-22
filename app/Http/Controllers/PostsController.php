d190f1f2bfe2a7e1503d973c6b440bb6b8a82e1d
moriken4320 <moriken.4320@gmail.com>
2021-01-22 15:39:17 +0900

    ルーティングの不具合修正

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use App\Subject;
+use Auth;
+use Validator;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
@@ -13,20 +16,46 @@ class PostsController extends Controller
         return view('post.index', ['posts'=>$posts]);
     }
 
-    public function create()
+    public function new()
     {
-        return view('post.create');
+        return view('post.new');
     }
     
-    public function store()
+    public function create(Request $request)
     {
+        //バリデーション（入力値チェック）
+        $validator = Validator::make($request->all(), [
+            'subject_title' => 'required|string|max:15|unique:subjects',
+            'study_time' => 'required|integer|min:1',
+            'text' => 'required|string|max:250',
+        ]);
 
+        //バリデーションの結果がエラーの場合
+        if ($validator->fails())
+        {
+          return redirect()->back()->withErrors($validator->errors())->withInput();
+        }
+
+        // Subjectモデルの作成
+        $subject = new Subject;
+        $subject->name = $request->subject_title;
+        $subject->save();
+
+        // Postモデル作成
+        $post = new Post;
+        $post->subject_id = $subject->id;
+        $post->study_time = $request->study_time;
+        $post->text = $request->text;
+        $post->user_id = Auth::user()->id;
+        $post->save();
+
+        return redirect('/');
     }
     
-    public function edit($post)
+    public function edit($post_id)
     {
-        $post_instance = Post::find($post);
-        return view('post.edit', ['post'=>$post_instance]);
+        $post = Post::find($post_id);
+        return view('post.edit', ['post'=>$post]);
     }
 
     public function update($post)
@@ -34,10 +63,10 @@ class PostsController extends Controller
 
     }
 
-    public function destroy($post)
+    public function destroy($post_id)
     {
-        $post_instance = Post::find($post);
-        $post_instance->delete();
+        $post = Post::find($post_id);
+        $post->delete();
         return redirect()->back();
     }
 }
