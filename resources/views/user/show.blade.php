e3228835177cd42e2dd3b2fb73eebeef20fd94be
moriken4320 <moriken.4320@gmail.com>
2021-01-23 16:45:42 +0900

    ユーザー詳細画面の勉強時間の表示を修正

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('title', 'ユーザー詳細 / Effornal')
-    
+
 @include('header')
 
 @section('content')
@@ -12,10 +12,11 @@
       {{-- base64という形式の画像データを表示する --}}
       <img class="user-show-left-user-icon" src="data:image/png;base64,{{ $user->image }}" />
       @else
-      <img class="user-show-left-user-icon" src="{{ asset('/images/blank_profile.png') }}"/>
+      <img class="user-show-left-user-icon" src="{{ asset('/images/blank_profile.png') }}" />
       @endif
       <p class="user-show-left-user-name"><span class="font-weight-bold">{{ $user->name }}</span>さんのページ</p>
     </div>
+    {{-- ユーザー編集画面に遷移するボタン --}}
     @if (Auth::check() && $user->id == Auth::user()->id)
     <div class="user-show-right">
       <a href="{{ route('users.edit') }}">
@@ -24,18 +25,20 @@
     </div>
     @endif
   </div>
+  {{-- 勉強時間関連の情報欄 --}}
   <div class="mx-auto time-sum-wrap">
-    <p>総勉強時間：{{ $study_data['sum_study_time'] }}</p>
-    <p>1日の最大勉強時間：{{ $study_data['max_study_time'] }}</p>
-    <ul>
+    <p>総勉強時間： {{ intdiv($study_data['sum_study_time'], 60) }}h {{ $study_data['sum_study_time'] % 60 }}m</p>
+    <p>1日の最大勉強時間： {{ intdiv($study_data['max_study_time'], 60) }}h {{ $study_data['max_study_time'] % 60 }}m</p>
+    <ul style="background-color: gray">
       @foreach ($study_data['subjects'] as $subject)
-      <li>{{ $subject['name'] }}：{{ $subject['sum_study_time'] }}</li>
+      <li>{{ $subject['name'] }}： {{ intdiv($subject['sum_study_time'], 60) }}h {{ $subject['sum_study_time'] % 60 }}m</li>
       @endforeach
     </ul>
   </div>
+  {{-- 該当ユーザーの投稿データ --}}
   <div class="show-posts-list">
     @foreach ($posts as $post)
-      @include('post.post_card')
+    @include('post.post_card')
     @endforeach
   </div>
 </div>
