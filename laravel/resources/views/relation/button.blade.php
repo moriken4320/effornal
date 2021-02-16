9257eec9b73244b08d581a83f9b0ff7d3647de42
moriken4320 <moriken.4320@gmail.com>
2021-02-16 16:34:03 +0900

    リレーションボタンの修正

--- laravel/resources/views/relation/button.blade.php
+++ laravel/resources/views/relation/button.blade.php
@@ -9,11 +9,13 @@
   @elseif (Auth::user()->throwerCheck($user))
 
   <input type="hidden" name="_method" value="PUT">
+  <p class="text-right assist-text">申請を受けています</p>
   <button type="submit" class="btn btn-outline-primary relation-btn" formaction="{{ route('relations.follow', ['user'=>$user]) }}"><i class="fas fa-handshake"></i>フレンドに追加</button>
 
   @elseif (Auth::user()->receiverCheck($user))
 
   <input type="hidden" name="_method" value="DELETE">
+  <p class="text-right assist-text">相手の承認を待ちです</p>
   <button type="submit" class="btn btn-primary relation-btn" formaction="{{ route('relations.unFollow', ['user'=>$user]) }}"><i class="fas fa-handshake active"></i>フレンド申請中</button>
 
   @else
