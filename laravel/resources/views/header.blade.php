eb41cc5260c69feb37b70fdcd0ddad8244964a99
moriken4320 <moriken.4320@gmail.com>
2021-02-17 10:26:42 +0900

    ランキング画面に遷移するボタン作成

--- laravel/resources/views/header.blade.php
+++ laravel/resources/views/header.blade.php
@@ -8,53 +8,61 @@
 
   <ul class="navbar-nav ml-auto">
 
-    @guest
-    <li class="nav-item">
-      <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="{{ route('login') }}">ログイン</a>
-    </li>
-    @endguest
-
-    @auth
-    {{-- 投稿作成ボタン --}}
-    <a class="btn btn-success new-post-btn" href="{{ route('posts.new') }}">
-      <i class="fas fa-external-link-alt"></i>
-      <p class="d-none d-sm-block">投稿する</p>
+    {{-- ランキングボタン --}}
+    <a href="#" class="btn btn-warning header-btn">
+      <i class="fas fa-crown"></i>
+      <p class="d-none d-sm-block">ランキング</p>
     </a>
 
-    {{-- ユーザーアイコン --}}
-    <li class="nav-item dropdown mr-2">
-      <div class="nav-link" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        @if (Auth::user()->image)
-        {{-- base64という形式の画像データを表示する --}}
-        <img class="header-user-icon" src="data:image/png;base64,{{ Auth::user()->image }}" />
-        @else
-        <img class="header-user-icon" src="{{ asset('/images/blank_profile.png') }}"/>
-        @endif
-      </div>
-      {{-- ドロップダウンで表示 --}}
-      <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-        <button class="dropdown-item" type="button"
-        onclick="location.href='{{ route('users.show', ['user_id' => Auth::user()->id]) }}'">
-        マイページ
-        </button>
-        <div class="dropdown-divider"></div>
-        <button class="dropdown-item" type="button"
-        onclick="location.href='{{ route('friends.index') }}'">
-        フレンド
-        </button>
-        <div class="dropdown-divider"></div>
-        <button form="logout-button" class="dropdown-item" type="submit">
-          ログアウト
-        </button>
-      </div>
-    </li>
-    <form id="logout-button" method="POST" action="{{ route('logout') }}" style="display: none;">
-      {{ csrf_field() }}
-    </form>
-    {{-- /ドロップダウンで表示 --}}
+    {{-- ログアウト中に表示 --}}
+    @guest
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('login') }}">ログイン</a>
+      </li>
+    @endguest
+
+    {{-- ログイン中に表示 --}}
+    @auth
+      {{-- 投稿作成ボタン --}}
+      <a class="btn btn-success header-btn" href="{{ route('posts.new') }}">
+        <i class="fas fa-external-link-alt"></i>
+        <p class="d-none d-sm-block">投稿する</p>
+      </a>
+
+      {{-- ユーザーアイコン --}}
+      <li class="nav-item dropdown mr-2">
+        <div class="nav-link" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          @if (Auth::user()->image)
+          {{-- base64という形式の画像データを表示する --}}
+          <img class="header-user-icon" src="data:image/png;base64,{{ Auth::user()->image }}" />
+          @else
+          <img class="header-user-icon" src="{{ asset('/images/blank_profile.png') }}"/>
+          @endif
+        </div>
+        {{-- ドロップダウンで表示 --}}
+        <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+          <button class="dropdown-item" type="button"
+          onclick="location.href='{{ route('users.show', ['user_id' => Auth::user()->id]) }}'">
+          マイページ
+          </button>
+          <div class="dropdown-divider"></div>
+          <button class="dropdown-item" type="button"
+          onclick="location.href='{{ route('friends.index') }}'">
+          フレンド
+          </button>
+          <div class="dropdown-divider"></div>
+          <button form="logout-button" class="dropdown-item" type="submit">
+            ログアウト
+          </button>
+        </div>
+      </li>
+      <form id="logout-button" method="POST" action="{{ route('logout') }}" style="display: none;">
+        {{ csrf_field() }}
+      </form>
+      {{-- /ドロップダウンで表示 --}}
     @endauth
 
   </ul>
