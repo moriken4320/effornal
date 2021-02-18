24d434052c4370f3d817b08ea858f64694c88488
moriken4320 <moriken.4320@gmail.com>
2021-02-18 16:45:44 +0900

    メッセージ部分のビュー作成

--- laravel/resources/views/room/message/index.blade.php
+++ laravel/resources/views/room/message/index.blade.php
@@ -8,27 +8,41 @@
 @section('content')
 {{-- ルームを表示 --}}
 <div class="container col-xl-5 col-lg-7 col-md-10 col-sm-12 mx-auto">
+  @if (count($room_messages) == 0)
+    <div class="text-center mt-5">メッセージがありません</div>
+  @endif
   @foreach ($room_messages as $message)
   {{-- @include('common.user_card') --}}
   @if ($message->user == Auth::user())
-    <div class="d-flex flex-column align-items-end">
-      <p>{{ $message->message }}</p>
-      <p>{{ $message->created_at->format('Y-m-d H:i') }}</p>
+    <div class="message own">
+      <div class="message-top">
+        <div class="message-content">
+          {!! nl2br(e($message->message)) !!}
+        </div>
+      </div>
+      <div class="message-bottom">
+        <p class="message-time">{{ $message->created_at->format('Y-m-d H:i') }}</p>
+      </div>
     </div>
   @else
-    <div class="d-flex flex-column align-items-start">
-      @if ($message->user->image)
-      {{-- base64という形式の画像データを表示する --}}
-      <img class="relation-user-image" src="data:image/png;base64,{{ $message->user->image }}" alt="avatar" />
-      @else
-      <img class="relation-user-image" src="{{ asset('/images/blank_profile.png') }}" alt="avatar" />
-      @endif
-      <p>{{ $message->user->name }}</p>
-      <p>{{ $message->message }}</p>
-      <p>{{ $message->created_at->format('Y-m-d H:i') }}</p>
+    <div class="message other">
+      <div class="message-top">
+        @if ($message->user->image)
+        {{-- base64という形式の画像データを表示する --}}
+        <img class="message-user-image" src="data:image/png;base64,{{ $message->user->image }}" alt="avatar" />
+        @else
+        <img class="message-user-image" src="{{ asset('/images/blank_profile.png') }}" alt="avatar" />
+        @endif
+        <div class="message-content">{!! nl2br(e($message->message)) !!}</div>
+      </div>
+      <div class="message-bottom">
+        <p class="message-time">{{ $message->created_at->format('Y-m-d H:i') }}</p>
+      </div>
     </div>
   @endif
-  <hr>
   @endforeach
 </div>
+
+{{-- 入力フォーム --}}
+
 @endsection
